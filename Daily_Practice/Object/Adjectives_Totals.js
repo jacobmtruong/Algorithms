2ba13f6b6886c99f7ec36d32a99702060c6ec8a1
Jacob Truong <jacob.truong@ymail.com>
2022-10-03 11:14:28 -0700

    daily algo

---
+++ Daily_Practice/Object/Adjectives_Totals.js
@@ -0,0 +1,19 @@
+// Burglary Series (14): Adjectives Total
+// You call your spouse in anger and a "little" argument takes place. Count the total amount of insults used. Given an object of insults, return the total amount of insults used.
+
+// Examples
+// totalAmountAdjectives({ a: "moron" }) ➞ 1
+
+// totalAmountAdjectives({ a: "idiot", b: "idiot", c: "idiot" }) ➞ 3
+
+// totalAmountAdjectives({ a: "moron", b: "scumbag", c: "moron", d: "dirtbag" }) ➞ 4
+// Notes
+// The list will never be empty (no empty object).
+
+
+function totalAmountAdjectives (obj) {
+    return Object.keys(obj).length;
+}
+
+console.log(totalAmountAdjectives({a: "moron", b: "scumbag", c: "moron", d: "dirtbag"}));
+console.log(totalAmountAdjectives({a: "moron", b: "scumbag"}));
